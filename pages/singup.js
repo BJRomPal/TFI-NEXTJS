d020ae345b94768e83f4e800bcd20e528865a1c2
Mariano Miró <marianomiro77@gmail.com>
2022-07-28 21:30:04 -0300

    arreglos a la pagina de singup

--- pages/singup.js
+++ pages/singup.js
@@ -1,6 +1,7 @@
 import style from '../styles/singup.module.css';
 import Image from 'next/image';
 import imagen from '../public/ImagenSingUp.jpg';
+import Link from 'next/link';
 
 
 export default function Singup() {
@@ -10,15 +11,27 @@ return (
         <form className={style.singUpForm}>
           <fieldset>
             <legend className={style.formLabel} >REGÍSTRATE</legend>
-            <label htmlFor="nombre" className={style.inputLabel} >Nombre</label>
-            <input className={style.input} type="text" id="nombre" name="nombre" required />
-      
-            <label htmlFor="email" className={style.inputLabel}>Correo Electrónico</label>
-            <input className={style.input} type="email" id="email" name="email" required />
-
-            <label htmlFor="password" className={style.inputLabel}>Contraseña</label>
-            <input className={style.input} type="password" id="password" name="password" required />
+            <div className={style.inputContainer}>
+              <input className={style.input} type="text" id="nombre" name="nombre" required />
+              <label htmlFor="nombre" className={style.inputLabel} >Nombre</label>
+            </div>
+            <div className={style.inputContainer}>
+              <input className={style.input} type="email" id="email" name="email" required />
+              <label htmlFor="email" className={style.inputLabel}>Correo Electrónico</label>
+            </div>
+            <div className={style.inputContainer}>
+              <input className={style.input} type="password" id="password" name="password" required pattern="[A-Za-z0-9]{6,15}"/>
+              <label htmlFor="password" className={style.inputLabel}>Contraseña</label>
+            </div>
           </fieldset>
+          <button className={style.button} type="submit">Crea tu Cuenta</button>
+          <div className={style.divTienesCuenta}>
+              <span className={style.TienesCuenta}>¿Ya tienes una cuenta? 
+                <Link href='./signin'>
+                  <a className={style.iniciaSesion}> Inicia Sesión</a>
+                </Link>
+              </span>
+          </div>
         </form>
         <div className={style.imagenSingUp}>
           <Image 
