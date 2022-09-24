f1027ae8191bb614120802c1b60b14dd7c9ea607
Mariano Miró <marianomiro77@gmail.com>
2022-09-24 20:47:39 -0300

    Arreglando las conexiones

--- pages/api/inmuebles/[id].js
+++ pages/api/inmuebles/[id].js
@@ -4,6 +4,7 @@ import Aviso from '../../../models/Aviso'
 
 export default async (request, response) => {
     await dbConnect()
+    require("models/Inmueble");
     const aviso = await Aviso.findById(request.body.id).populate({path: 'inmueble_id'}).lean()
     response.json(aviso);
   }
